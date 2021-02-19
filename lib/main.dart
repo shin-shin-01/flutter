7cb2083a83eb8488a97b6530dd6a991a87abd099
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-19 16:28:21 +0900

    lineLogin function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:my_app/services_locator.dart';
 import 'package:my_app/services/navigation.dart';
@@ -7,8 +10,15 @@ import 'package:my_app/ui/sign_in/sign_in_view.dart';
 import 'package:my_app/ui/splash/splash_view.dart';
 import 'package:my_app/ui/root/root_view.dart';
 
-void main() {
+void main() async {
+  await DotEnv.load(fileName: ".env");
+
   setupServiceLocator();
+  // LINE LOGIN
+  WidgetsFlutterBinding.ensureInitialized();
+  LineSDK.instance.setup(env["LINE_CHANNEL_ID"]).then((_) {
+    print("LineSDK Prepared");
+  });
   runApp(MyApp());
 }
 
--- lib/ui/sign_in/sign_in_viewmodel.dart
+++ lib/ui/sign_in/sign_in_viewmodel.dart
@@ -1,4 +1,6 @@
 import 'package:stacked/stacked.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 
 import 'package:my_app/services_locator.dart';
 import 'package:my_app/services/navigation.dart';
@@ -7,7 +9,17 @@ import 'package:my_app/services/navigation.dart';
 class SignInViewModel extends BaseViewModel {
   final _navigation = servicesLocator<NavigationService>();
 
-  void submitSignInFake() {
-    _navigation.pushAndReplace(routeName: '/root');
+  void lineLogin() async {
+    try {
+      final result = await LineSDK.instance.login();
+      // user id -> result.userProfile.userId
+      // user name -> result.userProfile.displayName
+      // user avatar -> result.userProfile.pictureUrl
+      // etc...
+      _navigation.pushAndReplace(routeName: '/root');
+    } on PlatformException catch (e) {
+      // Error handling.
+      print(e);
+    }
   }
 }
