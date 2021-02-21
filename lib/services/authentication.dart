f954a7653bb6f9db967acd54048a8b90988df118
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-21 11:04:12 +0900

    createUser when line-login

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -3,12 +3,19 @@ import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 
 import 'package:my_app/services_locator.dart';
 import 'package:my_app/services/navigation.dart';
+import 'package:my_app/services/api.dart';
+
+import 'package:my_app/model/user.dart';
 
 class AuthService {
   final _navigation = servicesLocator<NavigationService>();
 
   String access_token = "";
   String get accessToken => access_token;
+  String display_name = "";
+  String get name => display_name;
+  String user_id = "";
+  String get uid => user_id;
 
   Future lineLogin() async {
     try {
@@ -17,6 +24,9 @@ class AuthService {
       // user avatar -> result.userProfile.pictureUrl
       // etc...
       access_token = result.accessToken.value;
+      display_name = result.userProfile.displayName;
+      user_id = result.userProfile.userId;
+
       _navigation.pushAndReplace(routeName: '/root');
     } on PlatformException catch (e) {
       // Error handling.
--- lib/ui/sign_in/sign_in_viewmodel.dart
+++ lib/ui/sign_in/sign_in_viewmodel.dart
@@ -3,12 +3,17 @@ import 'package:stacked/stacked.dart';
 
 import 'package:my_app/services_locator.dart';
 import 'package:my_app/services/authentication.dart';
+import 'package:my_app/services/api.dart';
 
 /// SignInViewModel
 class SignInViewModel extends BaseViewModel {
   final _auth = servicesLocator<AuthService>();
+  final _api = servicesLocator<APIService>();
 
   void login() async {
     await _auth.lineLogin();
+
+    /// TODO: User情報どこかにセットする必要あるかも
+    await _api.createUser();
   }
 }
