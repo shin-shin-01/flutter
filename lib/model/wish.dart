baada2f74ccb5fedea1d70161a76a095d1b18815
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-24 02:21:34 +0900

    add userName, categoryName

--- lib/model/wish.dart
+++ lib/model/wish.dart
@@ -2,6 +2,12 @@ class Wish {
   /// id
   final int id;
 
+  // ユーザ名
+  final String userName;
+
+  // カテゴリー名
+  final String categoryName;
+
   /// 名称
   final String name;
 
@@ -9,11 +15,13 @@ class Wish {
   final int star;
 
   /// Constructor
-  Wish({this.id, this.name, this.star});
+  Wish({this.id, this.userName, this.categoryName, this.name, this.star});
 
   factory Wish.fromJson(Map<String, dynamic> json) {
     return Wish(
         id: json['id'] as int,
+        userName: json['user_name'] as String,
+        categoryName: json['category_name'] as String,
         name: json['name'] as String,
         star: json['star'] as int);
   }
