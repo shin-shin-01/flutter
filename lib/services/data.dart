0b7cde3a0bb11806d22e2a02e03d88edc187781f
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-27 16:31:58 +0900

    Myfriends in data

--- lib/services/data.dart
+++ lib/services/data.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 import 'package:my_app/model/user.dart';
+import 'package:my_app/model/friend.dart';
 
 /// data from anywhere
 class DataService {
@@ -8,12 +9,19 @@ class DataService {
   String get accessToken => access_token;
 
   User Me = new User();
+  List<Friend> MyFriends = [new Friend()];
 
   User get getMe => Me;
+  List<Friend> get getMyFriends => MyFriends;
+
   Future saveUser(User user, [String accessToken]) async {
     Me = user;
     if (accessToken != null) {
       access_token = accessToken;
     }
+  }
+
+  Future saveFriends(List<Friend> friends) async {
+    MyFriends = friends;
   }
 }
