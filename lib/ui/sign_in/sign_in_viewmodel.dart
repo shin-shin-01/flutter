7e92b46e47ae4e93d9a80d9e7ac0e8ab3264ec17
Kaze Shindo <kaze.bknb@outlook.jp>
2021-03-04 17:30:42 +0900

    get all data in login

--- lib/ui/sign_in/sign_in_viewmodel.dart
+++ lib/ui/sign_in/sign_in_viewmodel.dart
@@ -3,19 +3,30 @@ import 'package:stacked/stacked.dart';
 
 import 'package:my_app/services_locator.dart';
 import 'package:my_app/services/authentication.dart';
+import 'package:my_app/services/navigation.dart';
 import 'package:my_app/services/api.dart';
 
 /// SignInViewModel
 class SignInViewModel extends BaseViewModel {
   final _auth = servicesLocator<AuthService>();
+  final _navigation = servicesLocator<NavigationService>();
   final _api = servicesLocator<APIService>();
 
   void login() async {
+    setBusy(true);
     await _auth.lineLogin();
-
-    /// TODO: User情報どこかにセットする必要あるかも
+    print("done: lineLogin");
     await _api.createUser();
+    print("done: createUser");
     await _api.getFriends();
+    print("done: getFriends");
     await _api.getCategories();
+    print("done: getCategories");
+    await _api.getWishes();
+    print("done: getWishes");
+    await _api.getFriendWishes();
+    print("done: getFriendWishes");
+
+    _navigation.pushAndReplace(routeName: '/root');
   }
 }
