4d4d7fb5a51199203e4ad1a7a898a58e9847b72f
Kaze Shindo <kaze.bknb@outlook.jp>
2021-03-04 13:19:59 +0900

    remove TodoList

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -230,14 +230,4 @@ class APIService {
 
     await _data.saveFriends(friends);
   }
-
-  /// getTodos Fakefunction
-  Future<List<Todo>> getTodos() async {
-    final fakeTodo = Todo(id: 1, title: "firstTodo", detail: "firstTodoDetail");
-    final fakeTodoSec =
-        Todo(id: 2, title: "secondTodo", detail: "secondTodoDetail");
-
-    final todos = [fakeTodo, fakeTodoSec];
-    return todos;
-  }
 }
